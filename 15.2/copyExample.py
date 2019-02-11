8b6caa4eb6523546866becc6d4c7e978d17041a5
Carsten Wulff <carsten.wulff@nordicsemi.no>
2019-02-11 13:13:23 +0100

    Fixed running on windows also

--- 15.2/copyExample.py
+++ 15.2/copyExample.py
@@ -2,12 +2,17 @@
 # 
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
+#
+# Tested on Windows with anaconda, python 3.6.5 and vscode terminal
+# Test on Mach with python 3.6??
 
 import os
 import sys
 import shutil
 import glob
 import re
+
+
 
 
 def copyPath(fromPath,toPath):
@@ -19,7 +24,7 @@ def copyPath(fromPath,toPath):
 def removeOtherThanSegger(path):
     #subdirs = [os.path.join(d, o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))]
     
-    dirList  = glob.glob(f"{path}/**/",recursive=True)
+    dirList  = glob.glob(f"{path}{os.path.sep}**{os.path.sep}",recursive=True)
     
     dirsToDelete = [folder for folder in dirList if re.search(r"arm|iar|Output",folder)]
     for d in dirsToDelete:
@@ -29,8 +34,8 @@ def removeOtherThanSegger(path):
 def getRemoteOrLocalPath(sdkPath,currentSeggerFileLocation,originalSeggerFileLocation,path):
     #- Try local first
     newPath = ""
-    localPath = os.path.normpath(currentSeggerFileLocation + "/" + path)
-    remotePath = os.path.abspath(originalSeggerFileLocation + "/" + path)
+    localPath = os.path.normpath(currentSeggerFileLocation + os.path.sep + path)
+    remotePath = os.path.abspath(originalSeggerFileLocation + os.path.sep+ path)
        
     if(os.path.exists(localPath)):
         newPath = path
@@ -38,6 +43,7 @@ def getRemoteOrLocalPath(sdkPath,currentSeggerFileLocation,originalSeggerFileLoc
         newPath = remotePath
 
     newPath = newPath.replace(sdkPath,"$(SDK)")
+    newPath = newPath.replace("\\","/")
     return newPath
 
 
@@ -47,6 +53,7 @@ def modifySeggerProject(sdkPath,projectPath,copiedPath,filename):
     shutil.copy(filename,f"{filename}.bak")
     originalSeggerFileLocation = os.path.dirname(os.path.normpath(projectPath + str(filename).replace(copiedPath,"")))
     currentSeggerFileLocation = os.path.dirname(filename)
+
     buffer = ""
     with open(filename,"r") as f:
         for line in f:
@@ -62,32 +69,28 @@ def modifySeggerProject(sdkPath,projectPath,copiedPath,filename):
                     newPath = getRemoteOrLocalPath(sdkPath,currentSeggerFileLocation,originalSeggerFileLocation,d)
                     if(newPath != ""):
                         paths += newPath + ";"
-        
                 line = re.sub(includeDirRegex,f"c_user_include_directories=\"{paths}\"",line)
-            
+                
+
             #- Replace file_names that have a "../../<path>" pattern
             fileNameRegex = r"\"(../../.*)\""
             mfile = re.search(fileNameRegex,line)
             if(not m and mfile):
                 newPath = getRemoteOrLocalPath(sdkPath,currentSeggerFileLocation,originalSeggerFileLocation,mfile.group(1))   
-            
                 if(newPath != ""):
                     line = re.sub(fileNameRegex,f"\"{newPath}\"",line)
             
 
-            #if(re.search(r"</solution>",line)):
-            #    line = f"<configuration  Name=\"Common\" environment_variables=\"nRF5_SDK_DIR = {sdkPath}\" />\n" + line
-            #- Gobble up lines
+        
             buffer += line + "\n"
                 
-     
     with open(filename,"w") as f:
         f.write(buffer)
 
         
 
 def findAndModifySeggerProject(sdkPath,projectPath,path):
-    sesProjects  = glob.glob(f"{path}/**/*.emProject",recursive=True)
+    sesProjects  = glob.glob(f"{path}{os.path.sep}**{os.path.sep}*.emProject",recursive=True)
     for f in sesProjects:
         modifySeggerProject(sdkPath,projectPath,path,f)
 
@@ -97,12 +100,9 @@ def main(sdkPath,fromPath, toPath):
     print(f"Removing non Segger Embedded Studio Projects")
     removeOtherThanSegger(toPath)
     print(f"Modifying paths in Segger Embedded Studio Projects")
-    projectPath = os.path.normpath(f"{fromPath}/")
+    projectPath = os.path.normpath(f"{fromPath}{os.path.sep}")
 
     findAndModifySeggerProject(os.path.abspath(sdkPath),projectPath,toPath)
-
-
-
 
 if(len(sys.argv) == 4):
     main(sys.argv[1],sys.argv[2],sys.argv[3])
